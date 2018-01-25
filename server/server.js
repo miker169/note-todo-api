c15ec4098ab2a6623cc1fd07303c2b7b497b4c7c
Mike <mike@helens-imac.home>
2018-01-25 07:02:22 +0000

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -20,6 +20,16 @@ app.post('/todos', (req, res) => {
   });
 });
 
+app.get('/todos', (req, res) => {
+  Todo.find().then((todos) => {
+    res.send({
+      todos
+    });
+  }, err => {
+    res.status(400).send(err);
+  })
+});
+
 
 // GET /todos/{id}
 
