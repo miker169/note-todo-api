d4cfdd76ff982f63d800074d65f3f67c4b4cab0f
Mike Rayner <michael.raynor@noexternalemail.hsbc.com>
2018-01-25 13:55:04 +0000

    Setup app for heroku

--- server/server.js
+++ server/server.js
@@ -8,6 +8,8 @@ const { User } = require('./models/user');
 
 const app = express();
 
+const port = process.env.PORT || 3000;
+
 app.use(bodyParser.json());
 
 app.post('/todos', (req, res) => {
@@ -44,8 +46,8 @@ app.get('/todos/:id', (req, res) => {
   }).catch(err => res.status(400).send());
 });
 
-app.listen(3000, () => {
-  console.log('Starting on port 3000');
+app.listen(port, () => {
+  console.log(`Starting on port ${port}`);
 });
 
 module.exports = { app };
