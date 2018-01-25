85283e4364a800434cc2b6ee439455c8f89a1c54
Mike Rayner <michael.raynor@noexternalemail.hsbc.com>
2018-01-25 13:30:21 +0000

    Add test cases for GET /todos:id

--- server/server.js
+++ server/server.js
@@ -36,7 +36,6 @@ app.get('/todos/:id', (req, res) => {
   if(!ObjectID.isValid(id)) {
     return res.status(404).send();
   }
-
   Todo.findById(id).then((todo) => {
     if(!todo) {
       return res.status(404).send();
