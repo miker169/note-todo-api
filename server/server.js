b7d5621246f6636c580d06de1d1a6d947997802a
Mike Rayner <michael.raynor@noexternalemail.hsbc.com>
2018-01-25 11:11:20 +0000

    Add /GET todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { ObjectID } = require('mongodb');
 
 const { mongoose } = require('./db/mongoose');
 const { Todo } = require('./models/todo');
@@ -30,8 +31,19 @@ app.get('/todos', (req, res) => {
   })
 });
 
+app.get('/todos/:id', (req, res) => {
+  const id = req.params.id;
+  if(!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
 
-// GET /todos/{id}
+  Todo.findById(id).then((todo) => {
+    if(!todo) {
+      return res.status(404).send();
+    }
+    res.send({todo});
+  }).catch(err => res.status(400).send());
+});
 
 app.listen(3000, () => {
   console.log('Starting on port 3000');
